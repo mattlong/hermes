2afe07ce7fef34400e8052a3b45c34d89a36a71c
Matt Long <matt@crocodoc.com>
2012-08-29 03:14:14 +0000

    massive overhaul of presence and invitation management

--- hermes/chatserver.py
+++ hermes/chatserver.py
@@ -1,4 +1,4 @@
-import sys, logging, json, xmpp, select, socket, re, time
+import sys, logging, json, xmpp, select, socket, re, time, traceback
 from datetime import datetime
 
 import hermes
@@ -17,7 +17,6 @@ class HermesBot(object):
 
         self.name = name
         self.params = params
-
         self.jid = xmpp.protocol.JID(self.params['JID'])
 
     def connect(self):
@@ -32,34 +31,55 @@ class HermesBot(object):
 
         #self.client.RegisterDisconnectHandler(self.on_disconnect)
         self.client.RegisterHandler('message', self.on_message)
-        self.client.sendInitPresence(requestRoster=1)
+        self.client.RegisterHandler('presence',self.on_presence)
+        self.client.sendInitPresence(requestRoster=0)
 
-        #roster =  self.client.getRoster()
-        #for jid in roster.keys():
-        #    print jid
-        #    print roster.getSubscription(jid)
+        roster =  self.client.getRoster()
+        for m in self.params['MEMBERS']:
+            self.invite_user(m, roster=roster)
 
-    def invite_user(self, jid, inviter=None):
-        if len(filter(lambda m: m['JID'] == jid, self.params['MEMBERS'])) > 0:
+    def get_member(self, jid, default=None):
+        member = filter(lambda m: m['JID'] == jid, self.params['MEMBERS'])
+        if len(member) == 1:
+            return member[0]
+        elif len(member) == 0:
+            return default
+        else:
+            raise Exception('Multple members have the same JID of [%s]' % (jid,))
+
+    def is_member(self, m):
+        if isinstance(m, basestring):
+            jid = m
+        else:
+            jid = m['JID']
+
+        return len(filter(lambda m: m['JID'] == jid and m['STATUS'] in ('ACTIVE', 'INVITED'), self.params['MEMBERS'])) > 0
+
+    def invite_user(self, new_member, inviter=None, roster=None):
+        roster = roster or self.client.getRoster()
+        jid = new_member['JID']
+
+        if jid in roster.keys() and roster.getSubscription(jid) == 'both':
+            new_member['STATUS'] = 'ACTIVE'
             if inviter:
                 self.send_message('%s is already a member' % (jid,), inviter)
         else:
+            new_member['STATUS'] = 'INVITED'
             self.broadcast('inviting %s to the room' % (jid,))
-            self.client.sendPresence(jid=jid, typ='subscribed')
 
+            #Add nickname according to http://xmpp.org/extensions/xep-0172.html
             subscribe_presence = xmpp.dispatcher.Presence(to=jid, typ='subscribe')
             if 'NICK' in self.params:
-                #http://xmpp.org/extensions/xep-0172.html
                 subscribe_presence.addChild(name='nick', namespace=xmpp.protocol.NS_NICK, payload=self.params['NICK'])
             self.client.send(subscribe_presence)
 
-            new_member = { 'JID': jid, 'NICK': jid.split('@')[0] }
+        if not self.is_member(new_member):
+            new_member.setdefault('NICK', jid.split('@')[0])
             self.params['MEMBERS'].append(new_member)
-            self.send_message('You have been invited to chat in %s' % (self.name,), new_member)
 
     def kick_user(self, jid):
         for member in filter(lambda m: m['JID'] == jid, self.params['MEMBERS']):
-            member['KICKED'] = True
+            member['STATUS'] = 'KICKED'
             self.send_message('You have been kicked from %s' % (self.name,), member)
 
             self.client.sendPresence(jid=member['JID'], typ='unsubscribed')
@@ -73,7 +93,6 @@ class HermesBot(object):
             debug('message on %s to %s: %s' % (self.name, to['JID'], body), quiet=quiet)
 
             message = xmpp.protocol.Message(to=to['JID'], body=body, typ='chat')
-
             if html_body:
                 html = xmpp.Node('html', {'xmlns': 'http://jabber.org/protocol/xhtml-im'})
                 html.addChild(node=xmpp.simplexml.XML2Node("<body xmlns='http://www.w3.org/1999/xhtml'>" + html_body.encode('utf-8') + "</body>"))
@@ -83,10 +102,7 @@ class HermesBot(object):
 
     def broadcast(self, body, html_body=None, exclude=()):
         debug('broadcast on %s: %s' % (self.name, body,))
-        for member in filter(lambda m:
-                not m.get('KICKED') and
-                m.get('RECEIVE', True) and
-                m not in exclude, self.params['MEMBERS']):
+        for member in filter(lambda m: m['STATUS'] == 'ACTIVE' and m not in exclude, self.params['MEMBERS']):
             debug(member['JID'], indent=2)
             self.send_message(body, member, html_body=html_body, quiet=True)
 
@@ -95,7 +111,8 @@ class HermesBot(object):
 
     def do_invite(self, sender, body, args):
         for invitee in args:
-            self.invite_user(invitee, inviter=sender)
+            new_member = { 'JID': invitee }
+            self.invite_user(new_member, inviter=sender)
 
     def do_kick(self, sender, body, args):
         if sender.get('ADMIN') != True: return
@@ -123,6 +140,21 @@ class HermesBot(object):
     def on_disconnect(self):
         debug('ERROR: Disconnected from server!')
         #debug(str(self.client.reconnectAndReauth()))
+
+    def on_presence(self, session, presence):
+        if presence.getType() == 'subscribe':
+            from_jid = presence.getFrom()
+            if self.is_member(from_jid.getStripped()):
+                debug('Acknowledging subscription request from [%s]' % (from_jid,))
+                self.client.sendPresence(jid=from_jid, typ='subscribed')
+                member = self.get_member(from_jid)
+                member['STATUS'] = 'ACTIVE'
+                self.broadcast('%s has accepted their invitation!' % (from_jid,))
+            else:
+                #TODO: show that a user has requested membership?
+                pass
+        else:
+            debug('Unhandled presence stanza of type [%s] from [%s]' % (presence.getType(), presence.getFrom()))
 
     def on_message(self, con, event):
         msg_type = event.getType()
@@ -197,8 +229,10 @@ def start_server(settings):
                 debug('ERROR: broken socket detected')
             else:
                 debug('ERROR: unknown socket error %d' % (ex.errno,))
-        except Exception, ex:
-            debug('ERROR: %s' % (ex.message,))
+        except Exception:
+            exc_info = sys.exc_info()
+            err_msg = traceback.format_exception(exc_info[0], exc_info[1], exc_info[2])
+            debug(err_msg)
             time.sleep(1)
 
 def _get_sockets(bots):
